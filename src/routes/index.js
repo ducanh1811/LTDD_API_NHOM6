91ad4bb8a8de5d0aaea290d14149ec1290247198
ducanh1811 <119037853+ducanh1811@users.noreply.github.com>
2023-05-11 11:56:04 +0700

    DUCANH

---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+const orderRouter = require("./order")
+const orderItemRouter = require("./orderItem")
+const productRouter = require("./product")
+const userRouter = require("./user")
+const categoryRouter = require("./category")
+const uploadFileRouter = require("./uploadFiles")
+const cartRouter = require("./cart")
+const cartItemRouter = require("./cartItem")
+
+function route(app) {
+    app.use('/bookstore/api/v1/orders', orderRouter)
+    app.use('/bookstore/api/v1/categories', categoryRouter)
+    app.use('/bookstore/api/v1/orderitems', orderItemRouter)
+    app.use('/bookstore/api/v1/products', productRouter)
+    app.use('/bookstore/api/v1/users', userRouter)
+    app.use('/bookstore/api/v1/', uploadFileRouter)
+    app.use("/bookstore/api/v1/cart", cartRouter);
+    app.use("/bookstore/api/v1/cartitems", cartItemRouter)
+}
+
+module.exports = route
